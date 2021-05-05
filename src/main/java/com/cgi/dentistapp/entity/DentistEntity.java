9d04bacdfc4d779ea56976e1dcea3288c2ad754e
agoidin <locosantjago@gmail.com>
2021-05-05 23:44:32 +0100

    Created Dentist Entity

---
+++ src/main/java/com/cgi/dentistapp/entity/DentistEntity.java
@@ -0,0 +1,60 @@
+package com.cgi.dentistapp.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+public class DentistEntity {
+
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
+    private long id;
+    private String firstName;
+    private String lastName;
+
+    public DentistEntity() {
+    }
+
+    public DentistEntity(long id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public DentistEntity(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
