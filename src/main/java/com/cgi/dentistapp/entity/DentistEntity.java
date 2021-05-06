40aa81c9dc45c34a8c1db6ece23dfc32ac177d60
agoidin <locosantjago@gmail.com>
2021-05-06 01:52:58 +0100

    Added Dentist Entity to represent dentists in DB

--- src/main/java/com/cgi/dentistapp/entity/DentistEntity.java
+++ src/main/java/com/cgi/dentistapp/entity/DentistEntity.java
@@ -16,29 +16,29 @@ public class DentistEntity {
             strategy = GenerationType.SEQUENCE,
             generator = "student_sequence"
     )
-    private long id;
+    private Long id;
     private String firstName;
     private String lastName;
 
     public DentistEntity() {
     }
 
-    public DentistEntity(long id, String firstName, String lastName) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
+//    public DentistEntity(Long id, String firstName, String lastName) {
+//        this.id = id;
+//        this.firstName = firstName;
+//        this.lastName = lastName;
+//    }
 
     public DentistEntity(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
     }
 
-    public long getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
