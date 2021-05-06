4ef7c6e85a42be4a0d20345f0d9687bcaefc1795
agoidin <locosantjago@gmail.com>
2021-05-06 01:52:23 +0100

    Added Dentist repository

---
+++ src/main/java/com/cgi/dentistapp/config/DentistConfig.java
@@ -0,0 +1,32 @@
+package com.cgi.dentistapp.config;
+
+import com.cgi.dentistapp.entity.DentistEntity;
+import com.cgi.dentistapp.repository.DentistRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+@EnableJpaRepositories(basePackages = "com.cgi.dentistapp.repository")
+public class DentistConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(
+            DentistRepository repository) {
+        return args -> {
+            repository.save(new DentistEntity(
+                    "Maria",
+                    "Gold"
+            ));
+
+            repository.save(new DentistEntity(
+                    "Antonio",
+                    "Silver"
+            ));
+        };
+    }
+}
